c1b100ef538b023c3e648b2301b09b0c9687cebf
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-05-18 00:35:59 +0800

    Small changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,13 +4,39 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
-
+  const computer = useGLTF('public/desktop_pc/scene.gltf')
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive
+        object = {computer.scene}
+        scale = {0.75}
+      />
     </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return(
+    <Canvas
+      frameloop="demand"
+      shadows 
+      camera={{position: [20, 3, 5], fov: 25}}      
+      gl={{preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false} 
+          maxPolarAngle={Math.PI/2}
+          minPolarAngle={Math.PI/2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   )
 }
 
